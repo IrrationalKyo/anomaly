7195ed77e5299613fafb70204a8af5cd26683345
Rick <rick@localhost.localdomain>
2017-11-03 19:31:08 -0500

    added memory_heatmap.py

---
+++ memory_heatmap/memory_heatmap.py
@@ -0,0 +1,16 @@
+import numpy as np
+import math
+
+''' memory_trace is a list of memories accesses in sequential order '''
+def creat_heatmap(memory_trace, addr_base, size, delta):
+
+    heatmap = np.zeros(size)
+    for addr in memory_trace:
+        offset = addr - addr_base
+        index = math.floor(offset/delta)
+        if index >= size:
+            break
+        heatmap[index] += 1
+    return heatmap
+
+
